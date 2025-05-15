4a8244121a73b2c18e3cbc5097ee5ce620d17ccf
baicorp <bagusatoki@gmail.com>
2025-05-15 18:46:13 +0700

    remove react-vite for frontend

--- frontend/src/component/index.ts
+++
@@ -1,4 +0,0 @@
-import LoginWithGithubButton from "./LoginWithGithubButton";
-import LoginWithGoogleButton from "./LoginWithGoogleButton";
-
-export { LoginWithGithubButton, LoginWithGoogleButton };
--- frontend/src/page/index.ts
+++
@@ -1,6 +0,0 @@
-import App from "./App";
-import SignUp from "./SignUp";
-import SignIn from "./SignIn";
-import About from "./About";
-
-export { App, SignUp, SignIn, About };
