2b43f02e857f9cb62dd4d56f4188b8a6a2d5c06f
baicorp <bagusatoki@gmail.com>
2025-05-15 18:27:43 +0700

    use global session_time_stamp

--- backend/lib/lucia.ts
+++ backend/lib/lucia.ts
@@ -2,11 +2,12 @@ import { Lucia, TimeSpan } from "lucia";
 import { db } from "./drizzle";
 import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
 import { usersTable, sessionTable } from "../db/schema";
+import { SESSION_TIME_SPAN_IN_SECONDS } from "../constant";
 
 const adapter = new DrizzleSQLiteAdapter(db, sessionTable, usersTable);
 
 export const lucia = new Lucia(adapter, {
-  sessionExpiresIn: new TimeSpan(30, "s"), // if user not active for 30 seconds, session will expire and user will be logged out
+  sessionExpiresIn: new TimeSpan(SESSION_TIME_SPAN_IN_SECONDS, "s"), // if user not active for 30 seconds, session will expire and user will be logged out
   sessionCookie: {
     name: "bai-auth-session",
     // expires: true, // if set to true the database session automatically deleted expired session
